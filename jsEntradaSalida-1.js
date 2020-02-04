404c9c2d04809132c49ebca65ec52835048a8da2
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-04 19:05:06 -0300

    mi primer commit
    mi primer commit

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,9 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+function mostrar()
+{
+	//var nombre;
+	//nombre=prompt("hola","nn");
+	//alert("miguel");
+	alert("Esto funciona de maravilla");
+}
+
