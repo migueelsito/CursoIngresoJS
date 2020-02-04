232d6a579343f3f73d6ff019e334c95b43989a23
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-04 19:07:00 -0300

    mi segundo commit
    mi segundo commit

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,14 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	//nombre=prompt("hola","NN");
+	//alert(nombre);
+	var nombre;
+	nombre="Juan";
+	//alert("Hola "+"mundo");
+	//alert("Hola"+nombre);
+	var edad;
+	edad=33;
+	alert("Hola "+nombre+"su edad: "+edad);
 }
 
