14914ee330a133529261c819355f63248c3b334e
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-06 14:54:44 -0300

    Ejercicio 2

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,19 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	//var nombre;
+	//var edad;
+
+	//nombre=prompt("¿Cómo te llamas?","");
+	//edad=prompt("¿Cuántos años tienes?","0");
+
+	//alert("Hola, "+nombre+" de "+edad+" años");
+
+	var dato;
+
+	dato=prompt("Escribe algo: ");
+
+	alert("Escribiste esto: "+dato);
+}
+
