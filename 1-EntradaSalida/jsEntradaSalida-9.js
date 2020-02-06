55a7d2b841dc43f002627067d748c80fd4331d1c
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-06 17:33:42 -0300

    Ejercicio 9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,15 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var Sueldo;
+	var Resultado;
 
+	Sueldo=document.getElementById('sueldo').value;
+
+	Sueldo=parseInt(Sueldo);
+
+	Resultado=Sueldo+Sueldo*10/100;
+
+	document.getElementById('resultado').value=Resultado;
 	
 }
