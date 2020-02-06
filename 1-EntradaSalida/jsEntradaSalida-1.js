52cb4c2998052260af75c22b75ea252e1d5c2e60
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-06 15:02:18 -0300

    Primer ejercicio

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,9 +1,6 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	//var nombre;
-	//nombre=prompt("hola","nn");
-	//alert("miguel");
 	alert("Esto funciona de maravilla");
 }
 
