a8c16a9870f68825153919efbc85d4e3fc338ff4
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-06 17:17:12 -0300

    Ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,69 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primerNumero;
+	var segundoNumero;
+	var suma;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	suma=primerNumero+segundoNumero;
+
+	alert("la Suma es "+suma);
 }
 
 function restar()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resta;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	resta=primerNumero-segundoNumero;
+
+	alert("la Resta es "+resta);
 }
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var multiplicación;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	multiplicación=primerNumero*segundoNumero;
+
+	alert("la Multiplicación es "+multiplicación);
 }
 
 function dividir()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var división;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	división=primerNumero/segundoNumero;
+
+	alert("la División es "+división);
 }
 
