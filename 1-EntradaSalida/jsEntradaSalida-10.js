0a365bba88039e37f45e25c12e3baecba309f807
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-06 17:39:21 -0300

    Ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var Importe;
+	var Resultado;
+
+	Importe=document.getElementById('importe').value;
+	Importe=parseInt(Importe);
+
+	Resultado=Importe-Importe*25/100;
+
+	document.getElementById('resultado').value=Resultado;
 }
