26be15f24a5a8081f5ed5707344b4e1185a0653e
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-08 11:34:33 -0300

    Ejercicio 2 del Parcial

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
@@ -8,11 +8,13 @@ function mostrar()
  	var promedio;
 
  	nombre1=prompt("Primera persona, escriba su nombre","");
- 	peso1=prompt("Ahora escriba su peso","0");
- 	nombre2=prompt("Segunda persona, escriba su nombre");
- 	peso2=prompt("Segunda persona, escriba su peso","0");
 
+ 	peso1=prompt("Ahora escriba su peso","0");
  	peso1=parseInt(peso1);
+
+ 	nombre2=prompt("Segunda persona, escriba su nombre");
+
+ 	peso2=prompt("Segunda persona, escriba su peso","0");
  	peso2=parseInt(peso2);
 
  	pesosSumados=peso1+peso2;
