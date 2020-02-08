5856b6acdea76d1228704cc2d72b5f1bb6c41768
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-08 01:23:08 -0300

    Ejercicio 1 del Parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perímetro;
 
+	ancho=prompt("Escriba el ancho del rectángulo: ");
+	largo=prompt("Escriba el largo del rectángulo: ");
+
+	ancho=parseInt(ancho);
+	largo=parseInt(largo);
+
+	perímetro=(ancho+largo)*2;
+
+	alert("El perímetro del rectángulo es: "+perímetro);
 }
