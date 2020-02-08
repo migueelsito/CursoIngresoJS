97bb9cf90b19d9068fc291035913fac4854b52f2
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-08 00:46:26 -0300

    Ejercicio 10 y 11

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,6 +4,7 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	//EJERCICIO 10
 	var Importe;
 	var Resultado;
 
@@ -13,4 +14,20 @@ function mostrarAumento()
 	Resultado=Importe-Importe*25/100;
 
 	document.getElementById('resultado').value=Resultado;
+
+	//EJERCICIO 11
+	//var Importe;
+	//var Descuento;
+	//var Resultado;
+
+	//Importe=prompt("Escriba el importe: ","0");
+	//Descuento=prompt("Escriba el descuento: ","0");
+
+	//Importe=parseInt(Importe);
+	//Descuento=parseInt(Descuento);
+	//Resultado=parseInt(Resultado);
+
+	//Resultado=Importe-Importe*Descuento/100;
+
+	//alert("El resultado es "+Resultado);
 }
