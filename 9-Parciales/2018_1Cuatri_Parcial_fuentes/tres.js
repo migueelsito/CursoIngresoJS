8c6e20a5645e236a131570e3aea541324fd8305d
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-08 01:36:15 -0300

    Ejercicio 3 del Parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
+	var Precio;
+	var PorcentajeDescuento;
+	var Resultado;
 
+	Precio=prompt("Escriba el precio: ","0");
+	PorcentajeDescuento=prompt("Escriba el porcentaje de descuento: ","0");
+
+	Precio=parseInt(Precio);
+	PorcentajeDescuento=parseInt(PorcentajeDescuento);
+	Resultado=parseInt(Resultado);
+
+	Resultado=Precio-Precio*PorcentajeDescuento/100;
+
+	document.getElementById('elPrecioFinal').value=Resultado;
 }
