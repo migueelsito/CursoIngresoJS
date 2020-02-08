480effd1e5a0c6241033b0641f0481a81d08b86d
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-08 01:57:41 -0300

    Ejercicio 1 del Parcial

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+	var longitudLados;
+	var perímetro;
 
+	longitudLados=prompt("Escribe la longitud de los lados del rectángulo equilátero: ","0");
+
+	longitudLados=parseInt(longitudLados);
+
+	perímetro=3*longitudLados;
+
+	alert("El perímetro es: "+perímetro);
 }
