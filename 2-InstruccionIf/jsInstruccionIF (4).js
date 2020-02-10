96866f0888d0b6042b99aabc1a66f43c32a708cb
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-10 20:21:01 -0300

    Ejercicio 4

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,16 @@
 function mostrar()
 {
+	var edad;
 //tomo la edad  
+	edad=document.getElementById('edad').value;
 
+	if(edad>12)
+	{
+		if(edad<18)
+		{
+			alert("Es adolescente");
+		}
+	}
 
 
 }//FIN DE LA FUNCIÓN
