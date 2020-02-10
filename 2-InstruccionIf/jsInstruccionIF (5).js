cc72b0f299e30e5f20ca59774ef5f35882f40d7b
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-10 20:38:33 -0300

    Ejercicio 5

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
+	var edad;
 //tomo la edad  
+	edad=document.getElementById('edad').value;
 
+	if(edad<13||edad>17)
+	{
+		alert("No es adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
