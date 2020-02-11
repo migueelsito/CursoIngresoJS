33a670ebef63acbfd2e1fed208d70d4530867e6c
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-02-10 21:35:10 -0300

    Ejercicio 6

---
+++ jsInstruccionIF (6).js
@@ -0,0 +1,24 @@
+function mostrar()
+{
+	var edad;
+//tomo la edad  
+	edad=document.getElementById('edad').value;
+
+	if(edad>17)
+	{
+		alert("Usted es mayor de edad");
+	}
+	else
+	{
+		if(edad<13)
+		{
+			alert("Usted es niño");
+		}
+		else
+		{
+			alert("Usted es adolescente");
+		}
+	}
+
+
+}//FIN DE LA FUNCIÓN
