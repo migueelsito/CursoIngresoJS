c993d2ad2a250e4685731b5a95c4af80ed6f078c
migueelsito <junmigue@hotmail.comgit config --global user.name migueelsitogit commit git statusgit clone https://github.com/migueelsito/CursoIngresoJS.git+git config --global user.name migueelsitogit config --global user.email junmigue@hotmail.com>
2020-02-12 15:46:12 -0300

    Ejercicio 3

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,14 @@
 function mostrar()
 {
+	var edad;
 //tomo la edad  
+	edad=document.getElementById('edad');
 
-
+	if(edad>17)
+	{
+		alert(Usted es mayor de edad);
+	}else
+	{
+		alert(Usted es menor de edad);
+	}
 }//FIN DE LA FUNCIÓN
